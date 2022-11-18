f0dbec720854bff8a455eba79ca4ccba40e0e93d
zcfabra <christianwgnam@gmail.com>
2022-11-17 21:38:30 -0500

    replace audio buffer on effect

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -9,12 +9,13 @@ import React, { useEffect, useRef, useState } from 'react'
 import {DDSP, SPICE} from "@magenta/music"
 import { encodeWAV } from '../utils'
 import TrackView from '../components/trackview'
+import { applySustainControlChanges } from '@magenta/music/esm/core/sequences'
 
 const Home: NextPage = () => {
   const [model, setModel] = useState< SPICE>();
   const [ddspModel, setDdspModel] = useState<DDSP>();
 
-  const [savedBuffers, setSavedBuffers] = useState<AudioBuffer[]>([]);
+  const [tracks, setTracks] = useState<AudioBuffer[]>([]);
   const [resultBuffer, setResultBuffer] = useState<string | null>();
 
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -52,41 +53,33 @@ const addAudioElement = async (blob: Blob)=>{
   let buf = await blob.arrayBuffer();
   let audbuf =  await globalContext!.decodeAudioData(buf);
   console.log("AUDIO BUFFER: ", audbuf);
-  setSavedBuffers(prev=>[...prev , audbuf]);
+  setTracks(prev=>[...prev , audbuf]);
   const url = URL.createObjectURL(blob);
   console.log("--------",url)
   setSoundInput(url) 
 }
 
+const net = async ()=>{
+  const features= await model?.getAudioFeatures(tracks[selected!]);
+  const out = await ddspModel?.synthesize(features);
+  if (out == undefined){
+    console.log("BAILED");
+    return;
+  } else {
+    const encoded = encodeWAV(out, tracks[selected!].sampleRate);
+    const newAudioBuffer = await globalContext!.decodeAudioData(encoded.buffer as ArrayBuffer);
+    setTracks(prev=>{
+      prev[selected!] = newAudioBuffer!;
+      return [...prev] 
+    })
+  }
 
-
-// const thing = async ()=>{
-//   console.log("AUDIOREF: ",audioRef.current?.baseURI)
-//   console.log("AUDIO DATA: ", soundInput)
-
-
-
-//   const features = await model?.getAudioFeatures(savedBuffer!,0.0);
-//   console.log(features);
-//   if (typeof window !== "undefined"){
-    
-    
-//     const out = await ddspModel!.synthesize(features);
-
-//     const encoded = encodeWAV(out, 48000);
-
-//     let blob = new Blob([encoded], {type: "audio/wav"}), url = URL.createObjectURL(blob);
-//     console.log(blob);
-//     console.log(url) 
-//     setResultBuffer(url);
-//   }
-
-// }
+}
 return (
       <div className='w-full h-screen bg-gray-900 flex flex-col items-center pt-24'>
-        <div className='absolute top-0 left-0 ml-8 h-24 flex flex-col items-center justify-center'>
-
+        <div className='absolute top-0 left-0 ml-8 h-24 flex flex-row items-center justify-center'>
           <AudioRecorder onRecordingComplete={addAudioElement}/>
+          { selected != null && tracks.length !=0 && <button onClick={net}className='mx-4 w-32 h-12 bg-orange-500 rounded-md text-white'>Do</button>}
         </div>
 
         {soundInput && <div className='flex flex-col items-center'>
@@ -100,7 +93,7 @@ return (
             // <source src={resultBuffer} type="audio/webm;codescs=opus"/>
             </audio>} */}
 
-            <TrackView selected={selected} setSelected={setSelected} bpm={bpm}globalContext={globalContext!}tracks={savedBuffers}></TrackView>
+            <TrackView selected={selected} setSelected={setSelected} bpm={bpm}globalContext={globalContext!}tracks={tracks}></TrackView>
       </div>
     )
 
