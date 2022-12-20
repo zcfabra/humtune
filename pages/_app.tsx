bcb57487cfb963186742f4322cf4ffa6b7178fa4
zcfabra <christianwgnam@gmail.com>
2022-12-19 19:25:30 -0500

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
   <Head>
     <title>Humtune</title>
-    <link rel="shortcut icon" href="../public/favicon.ico" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
   </Head>
   <Component {...pageProps} />
   </>
